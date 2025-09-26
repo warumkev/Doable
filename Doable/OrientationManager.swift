9ebcda3c7ac2b7e686080ccf0805426956bb49d2
Kevin Tamme <k.tamme2003@gmail.com>
2025-09-26 11:39:32 +0200

    Remove OrientationManager class and related functionality

--- Doable/OrientationManager.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-final class OrientationManager: ObservableObject {
-    @Published var mask: InterfaceOrientationMask = .all
-    
-    func allowAll() { mask = .all }
-    func forcePortrait() { mask = .portrait }
-    func forceLandscape() { mask = .landscape }
-}
